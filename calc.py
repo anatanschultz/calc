3349a01e4e55db44ece404ec00ec769cbf431ff5
anatanschultz <anatanschultz@gmail.com>
2021-10-08 12:19:30 +0300

    fixed bad parameters in class Button

--- calc.py
+++ calc.py
@@ -1,6 +1,26 @@
 import pygame
 
-screen = pygame.display.set_mode((400, 400))
+calc_width = 400
+calc_height = 400
+
+screen = pygame.display.set_mode((calc_width, calc_height))
+
+
+class Button:
+    def __init__(self, pos, func):
+        self.size = calc_width // 4
+        self.pos = (pos[0] * self.size, pos[1] * self.size)
+        self.func = func
+
+        self.surface = pygame.Surface((self.size, self.size))
+        self.surface.fill((255, 255, 255))
+        self.rect = self.surface.get_rect()
+
+    def visualisation(self):
+        screen.blit(self.surface, self.pos)
+
+
+test_button = Button([2, 2], 1)
 
 calc_running = True
 
@@ -8,3 +28,12 @@ while calc_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             calc_running = False
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                mouse_pos = pygame.mouse.get_pos()
+                print(mouse_pos)
+
+    test_button.visualisation()
+
+    pygame.display.update()
+    pygame.time.delay(50)
