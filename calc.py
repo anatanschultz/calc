14ce61a80ff71dcee87a05250ce93e7e6023ef37
anatanschultz <anatanschultz@gmail.com>
2021-10-08 12:53:20 +0300

    add collide check. when mouse button is down, cell change self color

--- calc.py
+++ calc.py
@@ -1,5 +1,7 @@
 import pygame
 
+mouse_pos = pygame.Rect(-100, -100, 1, 1)
+
 calc_width = 400
 calc_height = 400
 
@@ -13,14 +15,20 @@ class Button:
         self.func = func
 
         self.surface = pygame.Surface((self.size, self.size))
-        self.surface.fill((255, 255, 255))
-        self.rect = self.surface.get_rect()
+        self.rect = pygame.Rect((self.pos[0], self.pos[1], self.size, self.size))
 
-    def visualisation(self):
-        screen.blit(self.surface, self.pos)
+    def visualisation(self, pos):
+        screen.blit(self.surface, self.rect)
+        if self.rect.colliderect(pos):
+            self.surface.fill((255, 0, 0))
+        else:
+            self.surface.fill((255, 255, 255))
 
 
-test_button = Button([2, 2], 1)
+buttons = []
+for x in range(4):
+    for y in range(4):
+        buttons.append(Button([x, y], 1))
 
 calc_running = True
 
@@ -30,10 +38,14 @@ while calc_running:
             calc_running = False
         if event.type == pygame.MOUSEBUTTONDOWN:
             if event.button == 1:
-                mouse_pos = pygame.mouse.get_pos()
+                mouse_pos = pygame.Rect(pygame.mouse.get_pos()[0],
+                                        pygame.mouse.get_pos()[1],
+                                        1,
+                                        1)
                 print(mouse_pos)
 
-    test_button.visualisation()
+    for i in buttons:
+        i.visualisation(mouse_pos)
 
     pygame.display.update()
     pygame.time.delay(50)
