05a5316808bd8a915c062eab7f743e74f757e88b
anatanschultz <anatanschultz@gmail.com>
2021-10-26 13:25:44 +0300

    have worked keys return cells optimize code

--- calc.py
+++ calc.py
@@ -3,36 +3,48 @@ import pygame
 mouse_pos = pygame.Rect(-100, -100, 1, 1)
 
 calc_width = 400
-calc_height = 400
+calc_height = 550
+block_count = 4
 
 screen = pygame.display.set_mode((calc_width, calc_height))
 
 
 class Button:
     def __init__(self, pos, func):
-        self.size = calc_width // 4
-        self.pos = (pos[0] * self.size, pos[1] * self.size)
-        self.func = func
+        self.size = calc_width // block_count
+        self.pos = (pos[0] * self.size, pos[1] * self.size + 150)
+        self.text = str(func)
 
         self.surface = pygame.Surface((self.size, self.size))
         self.rect = pygame.Rect((self.pos[0], self.pos[1], self.size, self.size))
 
-    def visualisation(self, pos):
+    def visualisation(self, pos):       # необходимо визуализировать цифры
         screen.blit(self.surface, self.rect)
         if self.rect.colliderect(pos):
             self.surface.fill((255, 0, 0))
+            print(self.text)
+            pos = pygame.Rect(-100, -100, 1, 1)
         else:
             self.surface.fill((255, 255, 255))
+        return pos
 
+
+key_massive = [7, 8, 9, "+",
+               4, 5, 6, "-",
+               1, 2, 3, "*",
+               "c", 0, "=", "/"]
 
 buttons = []
-for x in range(4):
-    for y in range(4):
-        buttons.append(Button([x, y], 1))
+
+for num, i in enumerate(key_massive):
+    y = num // block_count
+    x = num - (block_count * y)
+    buttons.append(Button([x, y], i))
 
 calc_running = True
 
 while calc_running:
+    keypress_active = False
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             calc_running = False
@@ -42,10 +54,10 @@ while calc_running:
                                         pygame.mouse.get_pos()[1],
                                         1,
                                         1)
-                print(mouse_pos)
+                keypress_active = True
 
     for i in buttons:
-        i.visualisation(mouse_pos)
+        mouse_pos = i.visualisation(mouse_pos)
 
     pygame.display.update()
-    pygame.time.delay(50)
+    pygame.time.delay(100)
--- main.py
+++ main.py
@@ -6,12 +6,12 @@ class Number:
 
     def take_number(self):
         for num, i in enumerate(self.massive):
-            number_dec = i * 10 ** (len(self.massive) - num - 1)
-            self.output += number_dec
+            number_decade = i * 10**(len(self.massive)-(num+1))
+            self.output += number_decade
         return self.output
 
 
-def create_massive():
+def create_massive():       # функция требует изменения на вызов математического действия
     massive = []
     enter_num = True
     while enter_num:
@@ -29,4 +29,4 @@ massive_y = create_massive()
 x = Number(massive_x)
 y = Number(massive_y)
 
-print(x.take_number() - y.take_number())
+print(x.take_number() + y.take_number())
